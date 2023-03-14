e983bf48d1eb02b37acec451d18605a51d7d20ac
fbiayna <ferranbiaynacosta@gmail.com>
2023-03-14 19:12:04 +0100

    Fixed

--- src/application/resolvers/mutations.ts
+++ src/application/resolvers/mutations.ts
@@ -1,11 +1,52 @@
+import { OrderState } from '../../domain/entities/orderState';
 import { OrderRepository } from '../../infrastructure/repositories/orderRepository';
+import populateItems from '../../infrastructure/utils/populateItems';
 
 export class Mutations {
   constructor(private readonly orderRepository: OrderRepository) {}
 
   get all() {
     return {
-      setOrderState: this.orderRepository.fetchOrder,
+      setOrderState: async (
+        _: unknown,
+        {
+          id,
+          state,
+          assignedTo,
+        }: {
+          id: string;
+          state: OrderState;
+          assignedTo?: string;
+        }
+      ) => {
+        try {
+          const order = await this.orderRepository.fetchOrder(
+            id,
+            populateItems
+          );
+
+          if (!order) {
+            throw new Error('order with ID ${id} not found');
+          }
+
+          if (order.state === state) {
+            return order;
+          }
+
+          if (state === OrderState.inProgress && !assignedTo) {
+            throw new Error('There is no assignedTo value');
+          }
+
+          return await this.orderRepository.setOrderState(
+            id,
+            state,
+            state === OrderState.inProgress && assignedTo ? assignedTo : null,
+            populateItems
+          );
+        } catch (error) {
+          throw new Error('Error setting order state');
+        }
+      },
     };
   }
 }
--- src/application/resolvers/queries.ts
+++ src/application/resolvers/queries.ts
@@ -1,12 +1,15 @@
 import { OrderRepository } from '../../infrastructure/repositories/orderRepository';
+import populateItems from '../../infrastructure/utils/populateItems';
 
 export class Queries {
   constructor(private readonly orderRepository: OrderRepository) {}
 
   get all() {
     return {
-      fetchOrder: this.orderRepository.fetchOrder,
-      fetchOrders: this.orderRepository.fetchOrders,
+      fetchOrder: async (_: unknown, args: { id: string }) =>
+        this.orderRepository.fetchOrder(args.id, populateItems),
+
+      fetchOrders: async () => this.orderRepository.fetchOrders(populateItems),
     };
   }
 }
